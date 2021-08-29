aecf771cd3294fed6b0f828b69e5d492cfca91c7
gparida <ganeshsanu111@gmail.com>
2021-08-29 15:49:09 -0500

    Preliminary reweighting script version

--- ReweightingNano/reweightScript.py
+++ ReweightingNano/reweightScript.py
@@ -10,6 +10,7 @@ import glob
 import ROOT
 from array import array
 from tqdm import tqdm
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Handle script for performing final reweighting of events')
@@ -31,4 +32,70 @@ if __name__ == "__main__":
             listOfBranchesToAdd=[]
             listOfBranchesWithVariationToAdd=[]
             for weight in theConfig.listOfWeights:
-            	
+                listOfBranchesToAdd.append(weight.name)
+                if weight.hasUpDownUncertainties:
+                    for uncertainity in weight.uncertaintyVariationList:
+                        listOfBranchesWithVariationToAdd.append(uncertainity)
+
+            finalWeightBranch = ["finalWeight"] 
+            fnames = theConfig.inputFile
+            outputDir="."
+            p = PostProcessor(outputDir, fnames,branchsel=None,modules=[addingWeightsConstr(theConfigModule,theConfig,listOfBranchesToAdd,listOfBranchesWithVariationToAdd,finalWeightBranch)], postfix="_CS_Gen_Weight_applied",noOut=False)
+            p.run()
+
+        except Exception as error:
+            print("Error! Details:")
+            traceback.print_exc()
+            numErrors+=1
+
+
+    if(numErrors > 0):
+        print("There were "+str(numErrors)+" errors(s).")
+        print("Please check them.")
+    else:
+        print("Completed with no errors!")
+
+
+
+class addingWeights(Module):
+    def __init__(self,configModule,configObject,listOfBranchesToAdd,listOfBranchesWithVariationToAdd,finalWeightBranch):
+        self.sampleModule = configModule
+        self.configObject = configObject
+        self.listOfBranchesToAdd = listOfBranchesToAdd
+        self.listOfBranchesWithVariationToAdd = listOfBranchesWithVariationToAdd
+        self.finalWeightBranch = finalWeightBranch
+
+        print (self.)
+        # add any arguments needed after the self and set them here
+        
+
+    def beginJob(self):
+        pass
+
+    def endJob(self):
+        pass
+
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+
+        self.out = wrappedOutputTree #--->Is this the name of output file
+        for name in self.listOfBranchesToAdd:
+            self.out.branch(name,"F")
+        for name in self.listOfBranchesWithVariationToAdd:
+            self.out.branch(name,"F")
+        self.out.branch(self.finalWeightBranch,"F")
+
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
+
+    def analyze(self, event):
+        #theFinalWeight[0] = 1.0
+        #for weight in self.configObject.listOfWeights:
+        #    weight.CalculateWeight(weight,theTree)
+        pass
+
+addingWeightsConstr = lambda: addingWeights(configModule,configObject,listOfBranchesToAdd,listOfBranchesWithVariationToAdd,finalWeightBranch)
+
+
