b86a223bd991b2d0d6ad88cb83b4d20156e2f42f
Ganesh Parida <ganeshsanu111@gmail.com>
2021-09-05 11:38:12 -0400

    Added changes to the channel sorting files

--- Analysis/channelSorter.py
+++ Analysis/channelSorter.py
@@ -1,28 +1,33 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 import ROOT
+import glob
 from particleClass import particle
+import argparse
+import traceback
+
 ROOT.PyConfig.IgnoreCommandLineOptions = True  #Find out what does this do ?
 
 class Channel(particle):
 	def __init__(self, channel):
 		self.channel = channel # Here we determine what kind of a particle it is
 		if self.channel == "tt":
-    		self.Tau = particle("Tau")
-    		self.FatJet = particle("FatJet")
+    			self.Tau = particle("Tau")
+    			self.FatJet = particle("FatJet")
 
 	def beginJob(self):
-        pass
+        	pass
 
-    def endJob(self):
-        pass
+    	def endJob(self):
+        	pass
 
-    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-    	self.Tau.beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-    	self.FatJet.beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    	def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    		self.Tau.beginFile(inputFile, outputFile, inputTree, wrappedOutputTree)
+    		self.FatJet.beginFile(inputFile, outputFile, inputTree, wrappedOutputTree)
 
-    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-        pass
+    	def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        	pass
 
 
    	def analyze(self, event):
@@ -30,30 +35,32 @@ class Channel(particle):
    		gtau = filter(lambda x: x.pt > 20 and abs(x.eta < 2.3), tau)
 		fatjet = Collection(event,"FatJet")
 		gfatjet = filter(lambda x: x.pt > 200 and abs(x.eta < 2.4), fatjet)
-		if (len(gtau)==2 and len(gfatjet)==1):
+		if (len(gtau)==2):
 			self.Tau.fillingStuff(event,gtau)
 			self.FatJet.fillingStuff(event,gfatjet)
 			return True
 		else:
 			return False		
 
-letsSortChannels = lambda: Channel(channelName)
+letsSortChannels = lambda: Channel(args.Channel)
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Script to Handle root file preparation to split into channels. Input should be a singular files for each dataset or data already with some basic selections applied')
 	parser.add_argument('--Channel',help="enter either tt or et or mut",required=True)
 	parser.add_argument('--inputLocation',help="enter the path to the location of input file set",default="")
+	args = parser.parse_args()
 
-try:
+	try:
 
- 	if args.Channel == "tt":
- 		fnames = glob.glob(args.inputLocation + "/*.root")
- 		outputDir = args.inputLocation
- 		outputbranches = "keep_and_drop.txt"
- 		cuts = "MET_pt>200 && PV_ndof > 4 && abs(PV_z) < 24 && sqrt(PV_x*PV_x+PV_y*PV_y) < 2"
- 		p = PostProcessor(outputDir, fnames, cut=cuts,branchsel=None,modules=[letsSortChannels(args.Channel)], postfix="_ttChannel",noOut=False,outputbranchsel=outputbranches)
-		p.run()
+ 		if args.Channel == "tt":
+ 			fnames = glob.glob(args.inputLocation + "/*.root")
+ 			outputDir = "/data/gparida/Background_Samples/bbtautauAnalysis/2016/TestOutput"
+ 			outputbranches = "keep_and_drop.txt"
+ 			cuts = "MET_pt>200 && PV_ndof > 4 && abs(PV_z) < 24 && sqrt(PV_x*PV_x+PV_y*PV_y) < 2"
+ 			p = PostProcessor(outputDir, fnames, cut=cuts,branchsel=None,modules=[letsSortChannels()], postfix="_ttChannel",noOut=False,outputbranchsel=outputbranches)
+			p.run()
 
-except Exception as error:
-	print("Error!")
+	except Exception as error:
+		print("Error!")
+		traceback.print_exc()
--- Analysis/particleClass.py
+++ Analysis/particleClass.py
@@ -10,43 +10,48 @@ class particle(Module):
 
 
 	def beginJob(self):
-        pass
+        	pass
 
-    def endJob(self):
-        pass
+    	def endJob(self):
+        	pass
 
 
-    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-    	if self.particleType == "Tau":
-    		self.out.branch("ngTau","I")
-    		self.out.branch("gTau_mass","F")
-    		self.out.branch("gTau_pt","F")
-    		self.out.branch("gTau_phi","F")
+    	def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+		self.out = wrappedOutputTree
+		print ("The output tree while branch creation ",self.out)
+    		if self.particleType == "Tau":
+    			self.out.branch("ngTau","I")
+    			self.out.branch("gTau_mass","F")
+    			self.out.branch("gTau_pt","F")
+    			self.out.branch("gTau_phi","F")
 
-    	if self.particleType == "FatJet"
-    		self.out.branch("ngFatJet","I")
+    		if self.particleType == "FatJet":
+    			self.out.branch("ngFatJet","I")
 			self.out.branch("gFatJet_mass","F")
 			self.out.branch("gFatJet_pt","F")
 			self.out.branch("gFatJet_phi","F")
 
 
 	def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-        pass
+        	pass
 
-    def fillingStuff(event,particleObject):
-    	if self.particleType == "Tau":
-    		self.out.fill("ngTau",len(particleObject))
-    		for i in range(len(particleObject)):
-    			self.out.fill("gTau_mass",particleObject[i].mass)
-    			self.out.fill("gTau_pt",particleObject[i].pt)
-    			self.out.fill("gTau_phi",particleObject[i].phi)
+    	def fillingStuff(self,event,particleObject):
+    		if self.particleType == "Tau":
+    			self.out.fillBranch("ngTau",len(particleObject))
+			print ("ngTau",len(particleObject))
+			print ("the output tree while branch filling", self.out)
+    			for i in range(len(particleObject)):
+				print ("gTau_pt",particleObject[i].pt)
+    				self.out.fillBranch("gTau_mass",particleObject[i].mass)
+    				self.out.fillBranch("gTau_pt",particleObject[i].pt)
+    				self.out.fillBranch("gTau_phi",particleObject[i].phi)
 
-    	if self.particleType == "FatJet":
-    		self.out.fill("ngFatJet",len(particleObject))
-    		for i in range(len(particleObject)):
-    			self.out.fill("gFatJet_mass",particleObject[i].mass)
-    			self.out.fill("gFatJet_pt",particleObject[i].pt)
-    			self.out.fill("gFatJet_phi",particleObject[i].phi)
+    		if self.particleType == "FatJet":
+    			self.out.fillBranch("ngFatJet",len(particleObject))
+    			for i in range(len(particleObject)):
+    				self.out.fillBranch("gFatJet_mass",particleObject[i].mass)
+    				self.out.fillBranch("gFatJet_pt",particleObject[i].pt)
+    				self.out.fillBranch("gFatJet_phi",particleObject[i].phi)
 
 
 
