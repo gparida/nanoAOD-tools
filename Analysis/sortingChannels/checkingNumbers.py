2b634daabe179ace15727688d5b1af570463b9f7
gparida <ganeshsanu111@gmail.com>
2021-09-25 14:03:06 -0500

    Update checkingNumbers.py

--- Analysis/sortingChannels/checkingNumbers.py
+++ Analysis/sortingChannels/checkingNumbers.py
@@ -168,7 +168,7 @@ def call_postpoc(files):
 		letsSortChannels = lambda: Channel(filename)
 		nameStrip=files.strip()
 		filename = (nameStrip.split('/')[-1]).split('.')[-2]
-		p = PostProcessor(outputDir,[files], cut=cuts,branchsel=None,modules=[letsSortChannels()], postfix=post,noOut=True,outputbranchsel=outputbranches,histFileName="Cutflow_"+filename+".root",histDirName="Plots")
+		p = PostProcessor(outputDir,[files], cut=cuts,branchsel=None,modules=[letsSortChannels()],noOut=True,outputbranchsel=outputbranches,histFileName="Cutflow_"+filename+".root",histDirName="Plots")
 		p.run()
 
 
@@ -200,7 +200,7 @@ if __name__ == "__main__":
 	outputDir = "."
 	outputbranches = "keep_and_drop.txt"
 	cuts = "&&".join(eventSelectionAND)
-	post ="_{}Channel".format(str(args.Channel))
+	#post ="_{}Channel".format(str(args.Channel))
 	argList = list()
 	filename =""
 	for file in fnames:
