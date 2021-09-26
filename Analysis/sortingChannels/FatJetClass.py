ea7d78326b74c8d8270df524c9fd970011b70c3b
gparida <ganeshsanu111@gmail.com>
2021-09-26 08:49:01 -0500

    Modified data type in branch list and FatJet Class

--- Analysis/sortingChannels/FatJetClass.py
+++ Analysis/sortingChannels/FatJetClass.py
@@ -1,7 +1,9 @@
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from branchesList import *
 from particleClass import particle
+
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True  #Find out what does this do ?
 
@@ -11,6 +13,12 @@ class FatJet(particle):
     
     def setUpBranches(self, wrappedOutputTree):
         super(FatJet,self).setUpBranches(wrappedOutputTree)
+        for branch in FatJetBranches:
+            wrappedOutputTree.branch("g{}_{}".format(self.particleType,branch),"F",lenVar="ng{}".format(self.particleType))
+
+
+
+
         wrappedOutputTree.branch("g{}_msoftdrop".format(self.particleType),"F",lenVar="ng{}".format(self.particleType))
     
     def fillBranches(self,wrappedOutputTree):
