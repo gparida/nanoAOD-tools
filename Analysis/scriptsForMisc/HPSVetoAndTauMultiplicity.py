9a9a4115612eed93c5355f45e407e575ff532202
gparida <ganeshsanu111@gmail.com>
2021-10-19 15:34:11 -0500

    Update HPSVetoAndTauMultiplicity.py

--- Analysis/scriptsForMisc/HPSVetoAndTauMultiplicity.py
+++ Analysis/scriptsForMisc/HPSVetoAndTauMultiplicity.py
@@ -20,19 +20,21 @@ class HPSVetoandMultiplicty(Module):
         self.writeHistFile=True
         self.filename = filename #filename passed cause we needed to count the events with zero divide errors
 	    #All these objects are common to all channels
-        self.boostedTauVLoose = particle("boostedTau")
-        self.HPSTauVloose = particle ("Tau")		
+        self.boostedTauVVLoose = particle("boostedTau")
+        self.HPSTauVVloose = particle ("Tau")		
 		
     def beginJob(self, histFile=None,histDirName=None):
         Module.beginJob(self,histFile,histDirName)
 		#Now lets define the cutflow histograms
 		#Starting to Di Tau channel selections
         self.totalMultiplicity =  ROOT.TH1F('totalMultiplicity', 'totalMultiplicity', 5, 0, 5)
-        self.leadingLooseMultiplcity =  ROOT.TH1F('leadinLooseMultiplcity', 'leadinLooseMultiplcity', 5, 0, 5)
+        self.leadingVLooseMultiplicity = ROOT.TH1F('leadingVLooseMultiplcity', 'leadingVLooseMultiplcity', 5, 0, 5)
+        self.leadingLooseMultiplcity =  ROOT.TH1F('leadingLooseMultiplcity', 'leadingLooseMultiplcity', 5, 0, 5)
         self.leadingMediumMultiplicity = ROOT.TH1F('leadingMediumMultiplicity', 'leadingMediumMultiplicity', 5, 0, 5)
         self.leadingTightMultiplicity = ROOT.TH1F('leadingTightMultiplicity', 'leadingTightMultiplicity', 5, 0, 5)
         
         self.addObject(self.totalMultiplicity)
+        self.addObject(self.leadingVLooseMultiplicity)
         self.addObject(self.leadingLooseMultiplcity)
         self.addObject(self.leadingMediumMultiplicity)
         self.addObject(self.leadingTightMultiplicity)
@@ -42,7 +44,7 @@ class HPSVetoandMultiplicty(Module):
         tau1 = ROOT.TLorentzVector(0.0,0.0,0.0,0.0)
         tau2=ROOT.TLorentzVector(0.0,0.0,0.0,0.0)
         tau1.SetPtEtaPhiM(tauCollectionObject.pt,tauCollectionObject.eta,tauCollectionObject.phi,tauCollectionObject.mass)
-        for loosetau in self.boostedTauVLoose.collection:
+        for loosetau in self.boostedTauVVLoose.collection:
             tau2.SetPtEtaPhiM(loosetau.pt,loosetau.eta,loosetau.phi,loosetau.mass)
             deltaR = tau1.DeltaR(tau2)
             if deltaR <= 0.02:
@@ -60,56 +62,68 @@ class HPSVetoandMultiplicty(Module):
         #self.boostedTauLoose.setupCollection(event)
         #self.boostedTauLoose.apply_cut(lambda x: (x.pt > 20) and (abs(x.eta) < 2.3) and (x.idMVAnewDM2017v2 & 4 == 4))
 
-        self.boostedTauVLoose.setupCollection(event)
-        self.boostedTauVLoose.apply_cut(lambda x: (x.pt > 20) and (abs(x.eta) < 2.3) and (x.idMVAnewDM2017v2 & 2 == 2))
+        self.boostedTauVVLoose.setupCollection(event)
+        self.boostedTauVVLoose.apply_cut(lambda x: (x.pt > 20) and (abs(x.eta) < 2.3) and (x.idMVAnewDM2017v2 & 1 == 1))
         
 
-        self.HPSTauVloose.setupCollection(event)
-        self.HPSTauVloose.apply_cut(lambda x: (x.pt > 20) and (abs(x.eta) < 2.3) and (x.idMVAnewDM2017v2 & 2 == 2))
+        self.HPSTauVVloose.setupCollection(event)
+        self.HPSTauVVloose.apply_cut(lambda x: (x.pt > 20) and (abs(x.eta) < 2.3) and (x.idMVAnewDM2017v2 & 1 == 1))
 
-        HPSVetoCollection = filter(self.HPStauVeto,self.HPSTauVloose.collection)
+        HPSVetoCollection = filter(self.HPStauVeto,self.HPSTauVVloose.collection)
 
-        print ("original HPS Taus = ",len(self.HPSTauVloose.collection),"Vetoed Taus = ",len(HPSVetoCollection))
+        print ("original HPS Taus = ",len(self.HPSTauVVloose.collection),"Vetoed Taus = ",len(HPSVetoCollection))
 
-        self.totalMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+        self.totalMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
 
-        if (len(self.boostedTauVLoose.collection)!=0 and len(HPSVetoCollection)!=0):
-            if ((self.boostedTauVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 4 == 4)):
-                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+        if (len(self.boostedTauVVLoose.collection)!=0 and len(HPSVetoCollection)!=0):
+            if ((self.boostedTauVVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 2 == 2)):
+                self.leadingVLooseMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
 
-            if ((self.boostedTauVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 4 == 4)):
-                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
-
-            if ((self.boostedTauVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 8 == 8)):
-                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
-
-            if ((self.boostedTauVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 8 == 8)):
-                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+            if ((self.boostedTauVVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (self.HPSTauVVloose[0].idMVAnewDM2017v2 & 2 == 2)):
+                self.leadingVLooseMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
             
-            if ((self.boostedTauVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 16 == 16)):
-                self.leadingTightMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
 
-            if ((self.boostedTauVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 16 == 16)):
-                self.leadingTightMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+            if ((self.boostedTauVVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 4 == 4)):
+                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+
+            if ((self.boostedTauVVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 4 == 4)):
+                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+
+            if ((self.boostedTauVVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 8 == 8)):
+                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+
+            if ((self.boostedTauVVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 8 == 8)):
+                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+            
+            if ((self.boostedTauVVLoose.collection[0].pt > HPSVetoCollection[0].pt) and (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 16 == 16)):
+                self.leadingTightMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+
+            if ((self.boostedTauVVLoose.collection[0].pt < HPSVetoCollection[0].pt) and (HPSVetoCollection[0].idMVAnewDM2017v2 & 16 == 16)):
+                self.leadingTightMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
 
 
-        if (len(self.boostedTauVLoose.collection)!=0 and len(HPSVetoCollection)==0):
-            if (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 4 == 4):
-                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
-            if (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 8 == 8):
-                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
-            if (self.boostedTauVLoose.collection[0].idMVAnewDM2017v2 & 16 == 16):
-                self.leadingTightMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+        if (len(self.boostedTauVVLoose.collection)!=0 and len(HPSVetoCollection)==0):
+            if (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 2 == 2):
+                self.leadingVLooseMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+            if (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 4 == 4):
+                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+            if (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 8 == 8):
+                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+            if (self.boostedTauVVLoose.collection[0].idMVAnewDM2017v2 & 16 == 16):
+                self.leadingTightMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
         
-        if (len(self.boostedTauVLoose.collection)==0 and len(HPSVetoCollection)!=0):
+        if (len(self.boostedTauVVLoose.collection)==0 and len(HPSVetoCollection)!=0):
+            if (HPSVetoCollection[0].idMVAnewDM2017v2 & 2 == 2):
+                self.leadingVLooseMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
+
             if (HPSVetoCollection[0].idMVAnewDM2017v2 & 4 == 4):
-                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+                self.leadingLooseMultiplcity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
             
             if (HPSVetoCollection[0].idMVAnewDM2017v2 & 8 == 8):
-                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+                self.leadingMediumMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
             
             if (HPSVetoCollection[0].idMVAnewDM2017v2 & 16 == 16):
-                self.leadingTightMultiplicity.Fill(len(self.boostedTauVLoose.collection)+len(HPSVetoCollection))
+                self.leadingTightMultiplicity.Fill(len(self.boostedTauVVLoose.collection)+len(HPSVetoCollection),event.FinalWeighting)
 
         self.totalMultiplicity.GetXaxis().SetTitle("Total Taus")
         self.totalMultiplicity.GetYaxis().SetTitle("Events")
@@ -130,6 +144,12 @@ class HPSVetoandMultiplicty(Module):
         self.leadingTightMultiplicity.GetYaxis().SetTitle("Events")
         self.leadingTightMultiplicity.SetLineColor(4)
         self.leadingTightMultiplicity.SetLineWidth(3)
+
+        self.leadingVLooseMultiplicity.GetXaxis().SetTitle("Total Taus")
+        self.leadingVLooseMultiplicity.GetYaxis().SetTitle("Events")
+        self.leadingVLooseMultiplicity.SetLineColor(5)
+        self.leadingVLooseMultiplicity.SetLineWidth(3)
+        
 
 
 
