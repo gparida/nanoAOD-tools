146a6a87cfdc273bb1642853dadcd3017d7933d5
gparida <ganeshsanu111@gmail.com>
2021-10-22 16:23:34 -0500

    Update controlPlotMTchannel.py

--- Analysis/controlPlotMaker/controlPlotMTchannel.py
+++ Analysis/controlPlotMaker/controlPlotMTchannel.py
@@ -254,7 +254,7 @@ def main():
     
 
     if args.year == '2016':
-        dataPath = '/data/gparida/Background_Samples/bbtautauAnalysis/2016/mut_Channel/'
+        dataPath = '/data/gparida/Background_Samples/bbtautauAnalysis/2016/mt_Channel/'
     elif args.year == '2017':
         dataPath = '/data/aloeliger/SMHTT_Selected_2017_Deep/'
     elif args.year == '2018':
