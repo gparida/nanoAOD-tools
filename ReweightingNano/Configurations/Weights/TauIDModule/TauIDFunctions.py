29d1a737a7c8a3680615d93c57862283af84f632
gparida <ganeshsanu111@gmail.com>
2021-11-18 17:30:02 -0600

    Update TauIDFunctions.py

--- ReweightingNano/Configurations/Weights/TauIDModule/TauIDFunctions.py
+++ ReweightingNano/Configurations/Weights/TauIDModule/TauIDFunctions.py
@@ -5,6 +5,6 @@ def CalculateTauIDWeight(self,theTree):
     for tau in range(len(theTree.allTau_pt)):
         tauVector =  ROOT.TLorentzVector()
         tauVector.SetPtEtaPhiM(theTree.allTau_pt[tau],theTree.allTau_eta[tau],theTree.allTau_phi[tau],theTree.allTau_mass[tau])
-        print ("pt value that is passed = ", tauVector.Pt(),theTree.allTau_genPartFlav[0],theTree.allTau_genPartFlav[1])
+        print ("pt value that is passed = ", tauVector.Pt(),theTree.allTau_genPartFlav[tau])
         self.value[0] = self.value[0] * self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.allTau_genPartFlav[tau])
         
