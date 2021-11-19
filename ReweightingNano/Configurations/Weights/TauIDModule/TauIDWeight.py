228271f236cfca3d6b3188bad44a5671bed9aac2
gparida <ganeshsanu111@gmail.com>
2021-11-18 19:07:08 -0600

    Update TauIDWeight.py

--- ReweightingNano/Configurations/Weights/TauIDModule/TauIDWeight.py
+++ ReweightingNano/Configurations/Weights/TauIDModule/TauIDWeight.py
@@ -5,6 +5,6 @@ import TauIDFunctions
 
 tauIDWeight_2016 = Weight()
 tauIDWeight_2016.name = 'TauIDWeight'
-tauIDWeight_2016.SFTool = TauIDSFTool("2016Legacy","MVAoldDM2017v2","VVLoose")
+tauIDWeight_2016.SFTool = TauIDSFTool("2016Legacy","MVAoldDM2017v2","VLoose")
 tauIDWeight_2016.CalculateWeight = TauIDFunctions.CalculateTauIDWeight
 tauIDWeight_2016.hasUpDownUncertainties = True
