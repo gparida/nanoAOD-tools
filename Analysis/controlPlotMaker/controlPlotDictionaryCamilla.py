474a4498266c18eea153f57939b53a462b0dbc65
gparida <ganeshsanu111@gmail.com>
2021-12-08 03:09:29 -0600

    Camilla Plot updates

---
+++ Analysis/controlPlotMaker/controlPlotDictionaryCamilla.py
@@ -0,0 +1,27 @@
+variableAxisTitleDictionary = {
+    "gFatJet_pt":"FatJet_pt",
+    "gFatJet_eta":"FatJet_eta",
+    "gFatJet_msoftdrop":"FatJet_msoftdrop",
+    "MET_pt":"MET",
+    "allTau_pt":"reconstructed_Tau_pt",
+    "allTau_eta":"reconstructed_Tau_eta",
+    "gMuon_pt":"Muon_pt",
+    "gMuon_eta":"Muon_eta",
+    "gElectron_pt":"Electron_pt",
+    "gElectron_eta":"Electron_eta",
+    "gDeltaR_LL":"DeltaR_LL",
+}
+
+variableSettingDictionary = {
+    "gFatJet_pt":"45,0,1500",
+    "gFatJet_eta":"30,-3.14,3.14",
+    "gFatJet_msoftdrop":"25,0.0,250.0",
+    "MET_pt":"50,0,1000",
+    "allTau_pt":"50,0,1000",
+    "allTau_eta":"30,-3.14,3.14",
+    "gMuon_pt":"50,0,1000",
+    "gMuon_eta":"30,-3.14,3.14",
+    "gElectron_pt":"50,0,1000",
+    "gElectron_eta":"30,-3.14,3.14",
+    "gDeltaR_LL":"15,0,1.5",
+}
