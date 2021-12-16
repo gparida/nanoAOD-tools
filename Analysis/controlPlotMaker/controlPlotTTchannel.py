ca46956d3661fbdebf99ac1292b0fd393e66339d
gparida <ganeshsanu111@gmail.com>
2021-12-16 15:27:06 -0600

    Update controlPlotTTchannel.py

--- Analysis/controlPlotMaker/controlPlotTTchannel.py
+++ Analysis/controlPlotMaker/controlPlotTTchannel.py
@@ -282,7 +282,7 @@ def main():
                         #"Flag_EcalDeadCellTriggerPrimitiveFilter",
                         #"Flag_BadPFMuonFilter",
                         #"Flag_eeBadScFilter"])
-                        default=["gFatJet_tau2/gFatJet_tau1<0.75"])
+                        default=["gFatJet_tau2/gFatJet_tau1<0.75","gDeltaR_LL<1.5"])
     parser.add_argument('--pause',
                         help='pause after drawing each plot to make it easier to view',
                         action='store_true')
