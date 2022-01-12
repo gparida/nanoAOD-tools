14fcbba25d5727eedac02da9fa70de602881ec48
gparida <ganeshsanu111@gmail.com>
2022-01-12 16:24:46 -0600

    Update genMeasurementRadionBranches.py

--- Analysis/sortingChannels/addingNewObservableBranches/genMeasurementRadionBranches.py
+++ Analysis/sortingChannels/addingNewObservableBranches/genMeasurementRadionBranches.py
@@ -66,7 +66,7 @@ class genMeasurementRadionBranches(Module):
     
 
 def call_postpoc(files):
-	genMeasurementRadion = lambda: genMeasurementRadion(filename)
+	genMeasurementRadion = lambda:genMeasurementRadionBranches(filename)
 	nameStrip=files.strip()
 	filename = (nameStrip.split('/')[-1]).split('.')[-2]
 	p = PostProcessor(outputDir,[files], cut=cuts,branchsel=outputbranches,modules=[genMeasurementRadion()], postfix=post,noOut=False,outputbranchsel=outputbranches)
