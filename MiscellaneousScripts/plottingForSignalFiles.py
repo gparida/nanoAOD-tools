15387dfe93a72e668ce2f2e0cadc92e205399e15
gparida <ganeshsanu111@gmail.com>
2022-01-16 00:57:36 -0600

    Update plottingForSignalFiles.py

--- MiscellaneousScripts/plottingForSignalFiles.py
+++ MiscellaneousScripts/plottingForSignalFiles.py
@@ -78,7 +78,8 @@ cmsLatex.SetTextFont(52)
 #cmsLatex.DrawLatex(0.1+0.08,0.95,"Preliminary")
 cmsLatex.DrawLatex(0.12+0.08,0.95,"Preliminary")
 lumiText = '16.81 fb^{-1}, 13 TeV'
-#cmsLatex.SetTextFont(42)
+cmsLatex.SetTextFont(42)
+cmsLatex.SetTextSize(0.04)
 cmsLatex.DrawLatex(0.55,0.95,lumiText)
 
 HiggsMass.SaveAs("HiggsM_Reso.pdf")
