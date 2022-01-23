5ba92e400849297d7c5748c19d8f147176e5e00e
gparida <ganeshsanu111@gmail.com>
2022-01-23 03:34:29 -0600

    Update plottingForSignalFiles.py

--- MiscellaneousScripts/plottingForSignalFiles.py
+++ MiscellaneousScripts/plottingForSignalFiles.py
@@ -119,7 +119,7 @@ Radionpt.SaveAs("RadionPt_Reso.pdf")
 
 
 #Plotting Higgs Mass
-HiggsMassbb = setUpHistrogram(Name="HiggsMassbb",LineColor=1,LineWidth=2,XTitle="Softdrop Mass^{Reco}/Higgs Mass^{True}",YTitle="Events",ttree=theTree,branch="gFatJet_msoftdrop",Nbins=40,min=0,max=2,cond="(FatJet_particleNetMD_Xbb/(FatJet_particleNetMD_Xbb+FatJet_particleNetMD_QCD))>0.87")
+HiggsMassbb = setUpHistrogram(Name="HiggsMassbb",LineColor=1,LineWidth=2,XTitle="Softdrop Mass^{Reco}/Higgs Mass^{True}",YTitle="Events",ttree=theTree,branch="gFatJet_msoftdrop/125",Nbins=40,min=0,max=2,cond="(FatJet_particleNetMD_Xbb/(FatJet_particleNetMD_Xbb+FatJet_particleNetMD_QCD))>0.87")
 
 HiggsbbMass = setUpCanvas("Soft Drop Mass")
 HiggsMassbb.SetMaximum(HiggsMassbb.GetMaximum()+50)
