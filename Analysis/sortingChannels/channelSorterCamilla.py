798b16227bc36ce5d3ae358f4068b8b8ab56881b
gparida <ganeshsanu111@gmail.com>
2022-02-03 22:36:47 -0600

    Update channelSorterCamilla.py

--- Analysis/sortingChannels/channelSorterCamilla.py
+++ Analysis/sortingChannels/channelSorterCamilla.py
@@ -227,10 +227,10 @@ class ChannelCamilla(Module):
 
 		self.Electron.setupCollection(event)
 		self.Electron.apply_cut(lambda x: x.mvaFall17V2Iso_WPL and (x.pt > 10))
-		self.Electron.collection = filter(self.Electron.relativeIso,self.Electron.collection)
+		#self.Electron.collection = filter(self.Electron.relativeIso,self.Electron.collection)
 
 		self.Muon.setupCollection(event)
-		self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1 and ((x.TauCorrPfIso/x.pt) < 0.25))
+		#self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1 and ((x.TauCorrPfIso/x.pt) < 0.25))
 
 		#filter Objects to remove those within the fatjet cone
 		self.Electron.collection = filter(self.FatJetConeIsolation,self.Electron.collection)
