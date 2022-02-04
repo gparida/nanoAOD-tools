419f2d15e5fc96bacb2bbe0e8bfff6f1cea52d36
gparida <ganeshsanu111@gmail.com>
2022-02-04 16:50:10 -0600

    Fixing branch issues

--- Analysis/sortingChannels/sortingTausCamilla.py
+++ Analysis/sortingChannels/sortingTausCamilla.py
@@ -40,10 +40,16 @@ class mergeTauCamilla(Module):
         self.out.fillBranch("{}_mass".format("allTau"),self.get_attributes("mass",colllist))
         self.out.fillBranch("{}_phi".format("allTau"),self.get_attributes("phi",colllist))
         self.out.fillBranch("{}_eta".format("allTau"),self.get_attributes("eta",colllist))
-        for branch in boostedTauBranches.values(): 
-            if (self.filename == "Data" and (branch[0] == "genPartFlav" or branch[0] =="genPartIdx")):
-                continue
-            self.out.fillBranch("{}_{}".format("allTau",branch[0]),self.get_attributes(branch[0],colllist))
+        for branch in boostedTauBranches.values():           
+            for branch2 in TauBranches.values():
+                if branch[0]==branch2[0]:
+                    if (self.filename == "Data" and (branch[0] == "genPartFlav" or branch[0] =="genPartIdx")):
+                        continue
+                    self.out.fillBranch("{}_{}".format("allTau",branch[0]),self.get_attributes(branch[0],colllist))
+                    break    
+            #if (self.filename == "Data" and (branch[0] == "genPartFlav" or branch[0] =="genPartIdx")):
+            #    continue
+            #self.out.fillBranch("{}_{}".format("allTau",branch[0]),self.get_attributes(branch[0],colllist))
 
     def get_attributes(self,variable,collList):
         list = []
