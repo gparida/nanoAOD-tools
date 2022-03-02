71d0800aec25e65bc218dafdca17cae6c9eb7e93
gparida <ganeshsanu111@gmail.com>
2022-03-02 15:28:03 -0600

    Removing isolation from the main script , fixing one QCD cross section

--- Analysis/sortingChannels/channelSorterCamilla.py
+++ Analysis/sortingChannels/channelSorterCamilla.py
@@ -227,12 +227,12 @@ class ChannelCamilla(Module):
 
 		self.Electron.setupCollection(event)
 		self.Electron.apply_cut(lambda x: x.mvaFall17V2Iso_WPL and (x.pt > 10))
-		self.Electron.collection = filter(self.Electron.relativeIso,self.Electron.collection)
+		#self.Electron.collection = filter(self.Electron.relativeIso,self.Electron.collection)
 
 		self.Muon.setupCollection(event)
 		#self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1 and ((x.TauCorrPfIso/x.pt) < 0.25))
-		#self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1)
-		self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1 and ((x.pfRelIso03_all/x.pt) < 0.25))
+		self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1)
+		#self.Muon.apply_cut(lambda x: x.pt > 10 and x.mvaId >= 1 and ((x.pfRelIso03_all/x.pt) < 0.25))
 
 		#filter Objects to remove those within the fatjet cone
 		self.Electron.collection = filter(self.FatJetConeIsolation,self.Electron.collection)
