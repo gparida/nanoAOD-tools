1e8c472d63c94036a78495c639f292b0ebd395ea
gparida <ganeshsanu111@gmail.com>
2022-05-12 11:22:35 +0200

    Update controlPlotTTchannel.py

--- Analysis/controlPlotMaker/controlPlotTTchannel.py
+++ Analysis/controlPlotMaker/controlPlotTTchannel.py
@@ -37,7 +37,7 @@ class MakeHistograms(object):
                  standardCutString,
                  additionalSelections,
                  histogramName,
-                 theWeight = 'FinalWeighting'):
+                 theWeight = 'btagWeighting*pileupWeighting*crossSectionWeighting'):
 
         theTree = theFile.Get('Events')
 
@@ -300,7 +300,7 @@ def main():
                         #"Flag_EcalDeadCellTriggerPrimitiveFilter",
                         #"Flag_BadPFMuonFilter",
                         #"Flag_eeBadScFilter"])
-                        default=["gDeltaR_LL<1.5","fastMTT_RadionLegWithMet_m>750","fastMTT_RadionLegWithMet_m<4250","gMVis_LL>0","(gFatJet_particleNetMD_Xbb/(gFatJet_particleNetMD_Xbb+gFatJet_particleNetMD_QCD))>0.87"])
+                        default=["gDeltaR_LL<1.5","fastMTT_RadionLegWithMet_m>750","fastMTT_RadionLegWithMet_m<4250","gMVis_LL>0"])
     parser.add_argument('--pause',
                         help='pause after drawing each plot to make it easier to view',
                         action='store_true')
